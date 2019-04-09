a1c1a073912e73e3db3137c48a2866e0eb507889
SahDoum <sahdoum@gmail.com>
2019-04-09 23:59:37 +0300

    Used cmd module in app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import sys
+import cmd
 from peewee import *
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
-db_location = '/Users/sah/Documents/BOTS/bot_statistics.db'
+db_location = 'bot_statistics.db'
 table_name = 'bot_stats'
 database = SqliteDatabase(db_location, **{})
 
@@ -38,73 +39,83 @@ bots = {}
 for i, t in enumerate(Actions.select().where(Actions.action == 'New bot')):
     bots[i+1] = t.app_id
     print('{}. {}'.format(i+1, t.app_id))
-i = input('Enter bot number: ')
-BOT_NAME = bots[int(i)]
-print('{} was selected.'.format(BOT_NAME))
 
 
-def log(from_datetime):
-    chats = {}
-    for action in Actions.select().where(Actions.app_id == BOT_NAME, Actions.date >= from_datetime):
-        chat = action.usr_name if action.chat_name is None else action.chat_name
-        if chat not in chats:
-            chats[chat] = {}
-        if action.action not in chats[chat]:
-            chats[chat][action.action] = 0
+class StatisticsCmd(cmd.Cmd):
+    prompt = 'stats$ '
 
-        chats[chat][action.action] += 1
+    def __init__(self, *args, **kwargs):
+        cmd.Cmd.__init__(self, *args, **kwargs)
+        i = input('Enter bot number: ')
+        self.BOT_NAME = bots[int(i)]
+        print('{} was selected.'.format(self.BOT_NAME))
 
-    text = '{} today statistics:'.format(BOT_NAME)
+    def log(self, from_datetime):
+        chats = {}
+        for action in Actions.select().where(Actions.app_id == self.BOT_NAME, Actions.date >= from_datetime):
+            chat = action.usr_name if action.chat_name is None else action.chat_name
+            if chat not in chats:
+                chats[chat] = {}
+            if action.action not in chats[chat]:
+                chats[chat][action.action] = 0
 
-    for chat in chats:
-        text += '\nIn chat {}:'.format(chat)
-        for act in chats[chat]:
-            text += '\n  {} {} times.'.format(act, chats[chat][act])
+            chats[chat][action.action] += 1
 
-    return text
+        text = ''
 
+        for chat in chats:
+            text += '\nIn chat {}:'.format(chat)
+            for act in chats[chat]:
+                text += '\n  {} {} times.'.format(act, chats[chat][act])
 
-while(True):
-    # cntrl-c to quit
-    try:
-        input_text = input('stats$ ')
+        return text
 
-    # завершение работы из консоли стандартным Ctrl-C
-    except KeyboardInterrupt as e:
-        print('\nKeyboard Interrupt. Good bye.\n')
-        sys.exit()
-
-    if input_text is '':
-        continue
-
-    input_text = input_text.split()
-
-    # list of all actions
-    if input_text[0] == 'all':
-        for action in Actions.select().where(Actions.app_id == BOT_NAME):
+    def do_all(self, arg):
+        for action in Actions.select().where(Actions.app_id == self.BOT_NAME):
             print(action)
-    # list of all chats
-    elif input_text[0] == 'chats':
-        for action in Actions.select().where(fn.Substr(Actions.action, 1, 5) == 'New c', Actions.app_id == BOT_NAME):
+
+    def do_chats(self, arg):
+        for action in Actions.select().where(fn.Substr(Actions.action, 1, 5) == 'New c', Actions.app_id == self.BOT_NAME):
             if action.chat_name is None:
                 print('User: {} Id: {}'.format(action.usr_name, action.usr_id))
             else:
                 print('Chat: {} Id: {}'.format(action.chat_name, action.chat_id))
-    # clean all actions
-    elif input_text[0] == 'clean':
+
+    def do_today(self, arg):
+        today = datetime.utcnow().date()
+        start = datetime(today.year, today.month, today.day)
+
+        text = '{} today statistics:'.format(self.BOT_NAME)
+        text += self.log(start)
+        print(text)
+
+    def do_week(self, arg):
+        today = datetime.utcnow().date()
+        start = today - timedelta(days=7)
+
+        text = '{} week statistics:'.format(self.BOT_NAME)
+        text += self.log(start)
+        print(text)
+
+    def do_clean(self, arg):
         Actions.delete().where(Actions.app_id == BOT_NAME, Actions.action != 'New bot').execute()
-    # change bot
-    elif input_text[0] == 'change':
+
+    def do_change(self, arg):
         for i, t in enumerate(Actions.select().where(Actions.action == 'New bot')):
             print('{}. {}'.format(i+1, t.app_id))
         i = input('Enter bot number: ')
-        BOT_NAME = bots[int(i)]
-    # today log
-    elif input_text[0] == 'today':
-        today = datetime.utcnow().date()
-        start = datetime(today.year, today.month, today.day)
-        print(log(start))
+        self.BOT_NAME = bots[int(i)]
 
-    # error
-    else:
-        print('error')
+    def do_exit(self, arg):
+        print("Exit")
+        return True
+
+    do_EOF = do_exit
+
+
+def parse(arg):
+    return tuple(map(int, arg.split()))
+
+
+if __name__ == "__main__":
+    StatisticsCmd().cmdloop()
